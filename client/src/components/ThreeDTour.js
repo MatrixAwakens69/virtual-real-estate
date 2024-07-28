3ba47e8f2bae41ab93009d2ecf47895f807d38b3
ponnanna123 <21cs02003@iitbbs.ac.in>
2024-07-28 12:09:34 +0530

    small changes

--- client/src/components/ThreeDTour.js
+++ client/src/components/ThreeDTour.js
@@ -1,10 +1,12 @@
 import { useRef, useEffect } from "react";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { FirstPersonControls } from "three/examples/jsm/controls/FirstPersonControls";
 
 function ThreeDTour({ modelPath }) {
   const mountRef = useRef(null);
+  const initialMousePosition = useRef({ x: 0, y: 0 });
 
   useEffect(() => {
     const scene = new THREE.Scene();
@@ -18,10 +20,50 @@ function ThreeDTour({ modelPath }) {
     renderer.setSize(window.innerWidth, window.innerHeight);
     mountRef.current.appendChild(renderer.domElement);
 
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;
-    controls.dampingFactor = 0.15;
-    controls.screenSpacePanning = false;
+    // const controls = new OrbitControls(camera, renderer.domElement);
+    // controls.enableDamping = true;
+    // controls.dampingFactor = 0.15;
+    // controls.screenSpacePanning = false;
+
+    const controls = new FirstPersonControls(camera, renderer.domElement);
+    controls.lookSpeed = 0.1;
+    controls.movementSpeed = 0.3;
+    controls.noFly = true;
+    controls.lookVertical = true;
+    controls.activeLook = false;
+
+    const enableLook = (event) => {
+      controls.activeLook = true;
+      initialMousePosition.current = { x: event.clientX, y: event.clientY };
+      renderer.domElement.style.cursor = "grabbing";
+    };
+    const disableLook = () => {
+      controls.activeLook = false;
+      renderer.domElement.style.cursor = "default";
+    };
+
+    const handleMouseMove = (event) => {
+      if (controls.activeLook) {
+        const deltaX = event.clientX - initialMousePosition.current.x;
+        const deltaY = event.clientY - initialMousePosition.current.y;
+        console.log(deltaX, deltaY);
+        const rotationX = deltaX * 0.002;
+        const rotationY = deltaY * 0.002;
+        camera.rotation.y -= rotationX;
+        camera.rotation.x -= rotationY;
+        initialMousePosition.current = { x: event.clientX, y: event.clientY };
+      }
+    };
+
+    renderer.domElement.addEventListener("mousedown", (event) => {
+      if (event.button === 0) enableLook(event);
+    });
+
+    renderer.domElement.addEventListener("mouseup", (event) => {
+      if (event.button === 0) disableLook();
+    });
+
+    renderer.domElement.addEventListener("mousemove", handleMouseMove);
 
     const loader = new GLTFLoader();
     loader.load(modelPath, (gltf) => {
@@ -33,7 +75,8 @@ function ThreeDTour({ modelPath }) {
 
     const animate = function () {
       requestAnimationFrame(animate);
-      controls.update();
+      //   controls.update();
+      controls.update(0.1);
       renderer.render(scene, camera);
     };
 
@@ -41,6 +84,9 @@ function ThreeDTour({ modelPath }) {
 
     return () => {
       mountRef.current.removeChild(renderer.domElement);
+      renderer.domElement.removeEventListener("mousedown", enableLook);
+      renderer.domElement.removeEventListener("mouseup", disableLook);
+      renderer.domElement.removeEventListener("mousemove", handleMouseMove);
     };
   }, [modelPath]);
 
